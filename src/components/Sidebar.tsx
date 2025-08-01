00dc8b069ff5a8e189f35dc45b4b69641a13bfc4
Altuğ ALTINTAŞ <altug.altintas59@gmail.com>
2025-08-01 22:54:04 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,30 +106,31 @@ function Sidebar({ open, onToggle }: { open: boolean; onToggle: () => void }) {
             </ListSubheader>
           ) : null
         }
-      > <ListItem  component={Link} to="/report2" sx={{ px: 2.5 }}>
+      >
+        {/*  <ListItem  component={Link} to="/report2" sx={{ px: 2.5 }}>
           <Tooltip title="Report2 '" placement="right" disableHoverListener={open}>
             <ListItemIcon sx={{ minWidth: 0, mr: open ? 2 : "auto", justifyContent: "center", color: "#333" }}>
               <BarChartIcon />
             </ListItemIcon>
           </Tooltip>
           {open && <ListItemText primary="İşlem Raporu" sx={{ color: "#333" }} />}
-        </ListItem>
+        </ListItem>*/}
         <ListItem  component={Link} to="/report/stockReport" sx={{ px: 2.5 }}>
           <Tooltip title="Stok Raporu" placement="right" disableHoverListener={open}>
             <ListItemIcon sx={{ minWidth: 0, mr: open ? 2 : "auto", justifyContent: "center", color: "#333" }}>
-              <WarehouseIcon />
+              <BarChartIcon />
             </ListItemIcon>
           </Tooltip>
           {open && <ListItemText primary="Stok Raporu" sx={{ color: "#333" }} />}
         </ListItem>
-        <ListItem  component={Link} to="/report" sx={{ px: 2.5 }}>
+       {/*  <ListItem  component={Link} to="/report" sx={{ px: 2.5 }}>
           <Tooltip title="Rapor İsim2" placement="right" disableHoverListener={open}>
             <ListItemIcon sx={{ minWidth: 0, mr: open ? 2 : "auto", justifyContent: "center", color: "#333" }}>
               <BarChartIcon />
             </ListItemIcon>
           </Tooltip>
           {open && <ListItemText primary="Rapor İsim2" sx={{ color: "#333" }} />}
-        </ListItem>
+        </ListItem>*/}
       </List>
     </Drawer>
   );
